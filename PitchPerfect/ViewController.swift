148a65fb7d23de5a7dbee9ce5faf2efbb50934e0
Narcis  Mihai <narcismihai@Narciss-Mac.local>
2016-02-01 20:46:09 +0200

    Added the logic for the stop and record buttons.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        recordingLabel.hidden = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startRecording(sender: AnyObject) {
+        recordingLabel.hidden = false;
+        recordingButton.enabled = false;
+    }
+    
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordingButton.enabled = true;
+        recordingLabel.hidden = true;
+    }
 
 }
 
