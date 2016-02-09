59f67ab963d803b1eb3e1f0d3cdfa5a277bb6998
Narcis  Mihai <narcismihai@Narciss-Mac.local>
2016-02-09 08:16:56 +0200

    Record and stop audio recording.

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Narcis  Mihai on 01/02/2016.
+//  Copyright © 2016 Narcis  Mihai. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class RecordSoundsViewController: UIViewController {
+
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    //Declared Globally
+    var audioRecorder:AVAudioRecorder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        recordingLabel.hidden = true;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startRecording(sender: AnyObject) {
+        recordingLabel.hidden = false;
+        recordingButton.enabled = false;
+        //record the audio
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        let recordingName = "SwiftAudioRecordingUdacity" + ".wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordingButton.enabled = true;
+        recordingLabel.hidden = true;
+        //stop the audio record
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        print("Stopped recording")
+        
+    }
+
+}
+
